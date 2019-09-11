7f6b89853c5ea3e23ece479e4679569c25df562d
George Rincon <georgerincon2@gmail.com>
2019-09-11 11:36:12 -0400

    added working schema and resolvers

--- index.js
+++ index.js
@@ -3,44 +3,66 @@ const bodyParser = require('body-parser');
 //exports a function we can use in the place where express expected a middle function
 //will take incoming requests and funnel them to the right resolvers
 const graphqlHttp = require('express-graphql');
-
+//get method to create shcema for models and mutations
 const { buildSchema } = require('graphql')
 const app = express();
 
+const events = [];
 app.use(bodyParser.json());
 
 app.use('/graphql', graphqlHttp({
     //query = Get
     //Mutation = changing (Create, Update, Delete)
+    //! next to type = required field / cannot be null
     schema: buildSchema(`
+        type Event {
+            _id: ID!
+            title: String!
+            description: String!
+            price: Float!
+            date: String!
+        }
         type RootQuery {
-            events: [String!]!
+            events: [Event!]!
+        }
+
+        input EventInput {
+            title: String!
+            description: String!
+            price: Float!
+            date: String!
         }
 
         type RootMutation {
-            createEvent(name: String): String
+            createEvent(eventInput: EventInput): Event
         }
         schema {
-            query: RootQuery
+            query: RootQuery 
             mutation: RootMutation
         }
     `),
-    //bundle of all resolvers we have
-    rootValue: { 
+    //bundle of all resolvers
+    rootValue: {
         events: () => {
-            return ['Romantic Cooking', 'Sailing', 'Coding']
+            return events
         },
         createEvent: (args) => {
-            const eventName = args.name;
-            return eventName;
-            
-        },
-        graphiql: true
-    }
+            const event = {
+                _id: Math.random().toString(),
+                title: args.eventInput.title,
+                description: args.eventInput.description,
+                //+ CONVERT TO FLOAT
+                price: +args.eventInput.price,
+                date: args.eventInput.date
+            };
+            events.push(event);
+
+            return event;
+        }
+
+    },
+    graphiql: true
 }))
 
-app.get('/', (req, res, next) => {
-    res.send('Hello Universe!');
-})
 
 app.listen(3000);
